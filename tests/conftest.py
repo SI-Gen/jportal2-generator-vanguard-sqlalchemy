e019adf75a4019db26e393b4b743d10004f6a14f
Dieter Rosch <dieter.rosch@gmail.com>
2022-05-16 11:58:45 +0200

    Updated generate_jportal.sh and conftest to use latest jportal docker

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,7 +25,7 @@ def generate_jportal():
     cwd = os.getcwd()
     parent = os.path.dirname(cwd)
     print(parent)
-    client.containers.run("bbdsoftware/jportal2",
+    client.containers.run("ghcr.io/si-gen/jportal2:latest",
                           "--inputdir=/local/tests/si \
                           --template-location=/local \
                           --flag SQLAlchemy.generateBuiltIns \
