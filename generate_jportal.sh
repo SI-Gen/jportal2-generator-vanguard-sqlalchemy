c40094aabc97a9beb5f98bd4b090c055a6a5aec9
Dieter Rosch <dieter.rosch@gmail.com>
2022-06-07 18:03:44 +0200

    Add support for testing XML

--- generate_jportal.sh
+++ generate_jportal.sh
@@ -1,10 +1,9 @@
 echo Running JPortal2 from ${PWD}
-docker pull ghcr.io/si-gen/jportal2:latest #To force a check for a newer "latest" on the server
+#docker pull ghcr.io/si-gen/jportal2:latest #To force a check for a newer "latest" on the server
 #docker run --rm -v ${PWD}:/local bbdsoftware/jportal2:latest \
 docker run --rm -v ${PWD}:/local ghcr.io/si-gen/jportal2:latest \
                       --inputdir=/local/tests/si \
                       --template-location=/local \
-                      --flag SQLAlchemy.generateBuiltIns \
                       --template-generator \
                         SQLAlchemy:/local/generated_sources/generated \
                       --builtin-generator \
